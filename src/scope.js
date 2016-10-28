e5aad5aac1e0692d6a9e9ba7efe9ac11adaf52fc
Hanruis <183631698@qq.com>
2016-10-28 16:00:36 +0800

    scope :  $apply invoke digest from root scope

--- src/scope.js
+++ src/scope.js
@@ -2,14 +2,16 @@
 'use strict';
 
 function Scope() {
-    this.$$watchers = [];
-    this.$$lastDirtyWatch = null;
-    this.$$asyncQueue = [];
+    this.$$watchers = []
+    this.$$lastDirtyWatch = null
+    this.$$asyncQueue = []
+    this.$$children = []
     // 使用这个的原因，可能是为了避免，在 $apply 的时候启动 $digest 。在 $digest 的时候 $apply
-    this.$$phase = null;
-    this.$$applyAsyncQueue = [];
-    this.$$applyAsyncId = null;
-    this.$$postDigestQueue = [];
+    this.$$phase = null
+    this.$$applyAsyncQueue = []
+    this.$$applyAsyncId = null
+    this.$$postDigestQueue = []
+    this.$$root = this
 }
 
 
@@ -102,30 +104,37 @@ Scope.prototype.$watchGroup = function (watchFns, listener) {
 Scope.prototype.$$digestOnce = function () {
     var self = this;
     var dirty;
-    _.forEachRight(this.$$watchers, function (watcher) {
-        if (watcher) {
-            try {
-                // 作者是把变量放在循环的外面。
-                // 其实不太懂为什么觉得其实放在里面也没什么关系。
-                // 如果从可读性来说，其实都一样吧。但是如果从性能来说，嗯，我觉得这个会好一点；不过现在 intel 的 
-                // cpu 有那么多的寄存器，好像也不是什么问题呃 (─.─|||)
-                var newValue = watcher.watchFn(self);
-                var oldValue = watcher.last;
-                if (!self.$$areEqual(newValue, oldValue, watcher.valueEqual)) {
-                    self.$$lastDirtyWatch = watcher;
-                    watcher.last = (watcher.valueEqual ? _.cloneDeep(newValue) : newValue);
-                    // 这里为什么要做条件表达式，来使得给到 listener 的 oldValue 不会是 initWatchVal
-                    watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue : oldValue), self);
-                    dirty = true;
-                } else if (self.$$lastDirtyWatch === watcher) {
-                    return false;
+    var continueLoop = true;
+    this.$$everyScope(function (scope) {
+        var newValue
+        var oldValue
+        _.forEachRight(scope.$$watchers, function (watcher) {
+            if (watcher) {
+                try {
+                    // 作者是把变量放在循环的外面。
+                    // 其实不太懂为什么觉得其实放在里面也没什么关系。
+                    // 如果从可读性来说，其实都一样吧。但是如果从性能来说，嗯，我觉得这个会好一点；不过现在 intel 的 
+                    // cpu 有那么多的寄存器，好像也不是什么问题呃 (─.─|||)
+                    newValue = watcher.watchFn(scope);
+                    oldValue = watcher.last;
+                    if (!scope.$$areEqual(newValue, oldValue, watcher.valueEqual)) {
+                        scope.$$lastDirtyWatch = watcher;
+                        watcher.last = (watcher.valueEqual ? _.cloneDeep(newValue) : newValue);
+                        // 这里为什么要做条件表达式，来使得给到 listener 的 oldValue 不会是 initWatchVal
+                        watcher.listenerFn(newValue, (oldValue === initWatchVal ? newValue : oldValue), scope);
+                        dirty = true;
+                    } else if (scope.$$lastDirtyWatch === watcher) {
+                        continueLoop = false
+                        return false;
+                    }
+                } catch (error) {
+                    console.error(error);
                 }
-            } catch (error) {
-                console.error(error);
             }
-        }
-    });
-    return dirty;
+        });
+        return continueLoop
+    })
+        return dirty;
 };
 
 Scope.prototype.$digest = function () {
@@ -186,22 +195,18 @@ Scope.prototype.$eval = function (expr, locals) {
 };
 
 Scope.prototype.$apply = function (applyFn) {
-    // applyFn(this);
-    // this.$digest();
     try {
         this.$beginPhase("$apply");
         return this.$eval(applyFn);
     } finally {
         this.$clearPhase();
-        this.$digest();
+        this.$$root.$digest();
     }
 };
 
 // 没有返回一个 destroy 函数么
 Scope.prototype.$evalAsync = function (evalFn) {
     var self = this;
-
-
     if (!self.$$phase && !self.$$asyncQueue.length) {
         setTimeout(function () {
             if (self.$$asyncQueue.length) {
@@ -263,10 +268,23 @@ Scope.prototype.$$postDigest = function (fn) {
 
 
 Scope.prototype.$new = function () {
-    // var child = Object.create(this)
-    // return child
     var ChildScope = function () { }
     ChildScope.prototype = this
     var child = new ChildScope()
+    this.$$children.push(child)
+    child.$$watchers = []
+    child.$$children = []
+    child.$$lastDirtyWatch = null
+    child.$$root = this.$$root
     return child
+}
+
+Scope.prototype.$$everyScope = function (fn) {
+    if (fn(this)) {
+        return this.$$children.every(function (child) {
+            return child.$$everyScope(fn)
+        })
+    } else {
+        return false
+    }
 }

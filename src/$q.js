82a731e6b33d621a756cf862e7397ceb72d79772
Hanruis <183631698@qq.com>
2016-11-07 11:01:16 +0800

    $q  resolve once

--- src/$q.js
+++ src/$q.js
@@ -2,8 +2,8 @@
 'use strict';
 
 function $QProvider() {
-    this.$get = function () {
-        
+    this.$get = ['$rootScope', function ($rootScope) {
+
         function Promise() {
             this.$$state = {};
         }
@@ -20,18 +20,30 @@ function $QProvider() {
         }
 
         Deferred.prototype.resolve = function (value) {
+            if (this.promise.$$state.status) {
+                return;
+            }
             this.promise.$$state.value = value;
+            this.promise.$$state.status = 1;
             scheduleProcessQueue(this.promise.$$state);
         }
 
+        function scheduleProcessQueue(state) {
+            $rootScope.$evalAsync(function () {
+                processQueue(state);
+            });
+        }
+
+        function processQueue(state) {
+            state.pending(state.value);
+        }
 
         function defer() {
             return new Deferred();
         }
 
         return {
-            defer:defer
+            defer: defer
         }
-
-    }
+    }]
 }

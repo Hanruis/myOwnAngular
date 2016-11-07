db7634877ff0c0ef261c33ec9244fd83f0de3ed6
Hanruis <183631698@qq.com>
2016-11-07 11:55:35 +0800

    $q, rejected

--- src/$q.js
+++ src/$q.js
@@ -8,16 +8,17 @@ function $QProvider() {
             this.$$state = {};
         }
 
-        Promise.prototype.then = function (onFullfilled) {
+        Promise.prototype.then = function (onFullfilled, onRejected) {
             this.$$state.pending = this.$$state.pending || [];
-            this.$$state.pending.push(onFullfilled);
+            this.$$state.pending.push([null, onFullfilled, onRejected]);
             if (this.$$state.status > 0) {
                 scheduleProcessQueue(this.$$state);
             }
         }
 
-
-
+        Promise.prototype.catch = function (onRejected) {
+            return this.then(null, onRejected);
+        }
 
         function Deferred() {
             this.promise = new Promise();
@@ -32,6 +33,15 @@ function $QProvider() {
             scheduleProcessQueue(this.promise.$$state);
         }
 
+        Deferred.prototype.reject = function (reason) {
+            if (this.promise.$$state.status) {
+                return
+            }
+            this.promise.$$state.value = reason;
+            this.promise.$$state.status = 2;
+            scheduleProcessQueue(this.promise.$$state);
+        }
+
         function scheduleProcessQueue(state) {
             $rootScope.$evalAsync(function () {
                 processQueue(state);
@@ -39,9 +49,16 @@ function $QProvider() {
         }
 
         function processQueue(state) {
-            _.forEach(state.pending, function (onFullfilled) {
-                onFullfilled(state.value);
+            _.forEach(state.pending, function (handlers) {
+                var fn = handlers[state.status];
+                if (_.isFunction(fn)) {
+                    fn(state.value);
+                }
             })
+            // 注意这里不能进行 state.status 的重置。
+            // 在设计中， resolve 可以在 promise callback 注册的前后中出现
+            // 在 then 和 resolve, reject 中，都会尝试执行 scheduleProcessQueue
+            // 其实就是两个状态的判断： 是否有 callback, 是否 resolve/reject 
             delete state.pending;
         }
 

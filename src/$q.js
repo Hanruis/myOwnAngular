b9417ed3516371f0598b3284898ea48b35a0cc13
Hanruis <183631698@qq.com>
2016-11-07 12:27:58 +0800

    增加 eslint

--- src/$q.js
+++ src/$q.js
@@ -1,9 +1,6 @@
 /* jshint globalstrict: true */
-'use strict';
-
 function $QProvider() {
     this.$get = ['$rootScope', function ($rootScope) {
-
         function Promise() {
             this.$$state = {};
         }
@@ -14,19 +11,19 @@ function $QProvider() {
             if (this.$$state.status > 0) {
                 scheduleProcessQueue(this.$$state);
             }
-        }
+        };
 
         Promise.prototype.catch = function (onRejected) {
             return this.then(null, onRejected);
-        }
+        };
 
-        promise.prototype.finally = function (callback) {
-            return this.then(function () {
+        Promise.prototype.finally = function (callback) {
+            return this.then(() => {
                 callback();
-            }, function () {
+            }, () => {
                 callback();
             });
-        }
+        };
 
         function Deferred() {
             this.promise = new Promise();
@@ -39,34 +36,34 @@ function $QProvider() {
             this.promise.$$state.value = value;
             this.promise.$$state.status = 1;
             scheduleProcessQueue(this.promise.$$state);
-        }
+        };
 
         Deferred.prototype.reject = function (reason) {
             if (this.promise.$$state.status) {
-                return
+                return;
             }
             this.promise.$$state.value = reason;
             this.promise.$$state.status = 2;
             scheduleProcessQueue(this.promise.$$state);
-        }
+        };
 
         function scheduleProcessQueue(state) {
-            $rootScope.$evalAsync(function () {
+            $rootScope.$evalAsync(() => {
                 processQueue(state);
             });
         }
 
         function processQueue(state) {
-            _.forEach(state.pending, function (handlers) {
-                var fn = handlers[state.status];
+            _.forEach(state.pending, (handlers) => {
+                const fn = handlers[state.status];
                 if (_.isFunction(fn)) {
                     fn(state.value);
                 }
-            })
+            });
             // 注意这里不能进行 state.status 的重置。
             // 在设计中， resolve 可以在 promise callback 注册的前后中出现
             // 在 then 和 resolve, reject 中，都会尝试执行 scheduleProcessQueue
-            // 其实就是两个状态的判断： 是否有 callback, 是否 resolve/reject 
+            // 其实就是两个状态的判断： 是否有 callback, 是否 resolve/reject
             delete state.pending;
         }
 
@@ -75,7 +72,7 @@ function $QProvider() {
         }
 
         return {
-            defer: defer
-        }
-    }]
+            defer
+        };
+    }];
 }

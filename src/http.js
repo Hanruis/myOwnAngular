d593f1e48a6ce868fd19b7e30d4921827421788a
Hanruis <183631698@qq.com>
2016-11-08 11:40:41 +0800

    $http: sending http request

--- src/http.js
+++ src/http.js
@@ -2,9 +2,30 @@
 
 
 function $HttpProvider() {
-    this.$get = ['$httpBackend', '$q', function ($httpBackend, $q) {
-        return function $http() {
+    this.$get = ['$httpBackend', '$q', '$rootScope', function ($httpBackend, $q, $rootScope) {
+        return function $http(config) {
             var d = $q.defer();
+
+            function done(status, response, statusText) {
+                status = Math.max(status, 0);
+                d[isSuccess(status) ? 'resolve' : 'reject']({
+                    status: status,
+                    data: response,
+                    statusText: statusText,
+                    config
+                });
+                if (!$rootScope.$$phase) {
+                    $rootScope.$apply();
+                }
+            }
+            // 这里为什么不考虑 302 的问题？
+            // 因为遇到了 302 ，浏览器会自行处理这个资源，不会去到我们的 js 代码中来处理。
+            function isSuccess(status) {
+                return status >= 200 && status < 300;
+            }
+
+            $httpBackend(config.method, config.url, config.data, done);
+
             return d.promise;
         };
     }];
--- test/http_spec.js
+++ test/http_spec.js
@@ -5,6 +5,7 @@
 describe('$http', function () {
     var $http;
     var xhr;
+    var requests;
     beforeEach(function () {
         publishExternalAPI();
         var injector = createInjector(['ng']);
@@ -13,6 +14,10 @@ describe('$http', function () {
 
     beforeEach(function () {
         xhr = sinon.useFakeXMLHttpRequest();
+        requests = [];
+        xhr.onCreate = function (req) {
+            requests.push(req);
+        };
     });
     afterEach(function () {
         xhr.restore();
@@ -25,5 +30,64 @@ describe('$http', function () {
         var result = $http({});
         expect(result).toBeDefined();
         expect(result.then).toBeDefined();
+    });
+    it('makes an XMLHttpRequest to given URL', function () {
+        $http({
+            method: 'POST',
+            url: 'http://teropa.info',
+            data: 'hello'
+        });
+        expect(requests.length).toBe(1);
+        expect(requests[0].method).toBe('POST');
+        expect(requests[0].url).toBe('http://teropa.info');
+        expect(requests[0].async).toBe(true);
+        expect(requests[0].requestBody).toBe('hello');
+    });
+    it('resolves promise when XHR result received', function () {
+        var requestConfig = {
+            method: 'GET',
+            url: 'http://teropa.info'
+        };
+        var response;
+        $http(requestConfig).then(function (r) {
+            response = r;
+        });
+        requests[0].respond(200, {}, 'Hello');
+        expect(response).toBeDefined();
+        expect(response.status).toBe(200);
+        expect(response.statusText).toBe('OK');
+        expect(response.data).toBe('Hello');
+        expect(response.config.url).toEqual('http://teropa.info');
+    });
+    it('rejects promise when XHR result received with error status', function () {
+        var requestConfig = {
+            method: 'GET',
+            url: 'http://teropa.info'
+        };
+        var response;
+        $http(requestConfig).catch(function (r) {
+            response = r;
+        });
+        requests[0].respond(401, {}, 'Fail');
+        expect(response).toBeDefined();
+        expect(response.status).toBe(401);
+        expect(response.statusText).toBe('Unauthorized');
+        expect(response.data).toBe('Fail');
+        expect(response.config.url).toEqual('http://teropa.info');
+    });
+    it('rejects promise when XHR result errors/aborts', function () {
+        var requestConfig = {
+            method: 'GET',
+            url: 'http://teropa.info'
+        };
+        var response;
+        $http(requestConfig).catch(function (r) {
+            response = r;
+        });
+        requests[0].onerror();
+        expect(response).toBeDefined();
+        expect(response.status).toBe(0);
+        expect(response.data).toBe(null);
+        expect(response.config.url).toEqual('http://teropa.info');
     });
 });

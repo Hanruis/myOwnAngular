523c8177e3fd3fd3770b10f98ef34eb5ee7f9ef5
Hanruis <183631698@qq.com>
2016-11-09 16:21:46 +0800

    $http response transforms

--- src/http.js
+++ src/http.js
@@ -27,10 +27,10 @@ function $HttpProvider() {
 
     this.$get = ['$httpBackend', '$q', '$rootScope', function ($httpBackend, $q, $rootScope) {
         function $http(requestConfig) {
-            var d = $q.defer();
             var config = _.extend({
                 method: 'GET',
-                transformRequest: defaults.transformRequest
+                transformRequest: defaults.transformRequest,
+                transformResponse:defaults.transformResponse
             }, requestConfig);
             config.headers = mergeHeaders(requestConfig);
 
@@ -47,7 +47,36 @@ function $HttpProvider() {
                 config.withCredentials = defaults.withCredentials;
             }
 
+            return sendReq(config, reqData).then(transformResponse, transformResponse);
 
+            function removeContentType(headers) {
+                _.forEach(config.headers, function (v, k) {
+                    if (k.toLowerCase() === 'content-type') {
+                        delete headers[k];
+                    }
+                });
+            }
+
+            function transformResponse(response) {
+                if (response.data) {
+                    response.data = transformData(
+                        response.data,
+                        response.headers,
+                        config.transformResponse,
+                        response.status
+                    );
+                }
+
+                if (isSuccess(response.status)) {
+                    return response;
+                } else {
+                    return $q.reject(response);
+                }
+            }
+        }
+
+        function sendReq(config, reqData) {
+            var d = $q.defer();
             $httpBackend(
                 config.method,
                 config.url,
@@ -56,8 +85,6 @@ function $HttpProvider() {
                 config.headers,
                 config.withCredentials
             );
-
-            return d.promise;
 
             function done(status, response, headerString, statusText) {
                 status = Math.max(status, 0);
@@ -72,95 +99,91 @@ function $HttpProvider() {
                     $rootScope.$apply();
                 }
             }
-            // 这里为什么不考虑 302 的问题？
-            // 因为遇到了 302 ，浏览器会自行处理这个资源，不会去到我们的 js 代码中来处理。
-            function isSuccess(status) {
-                return status >= 200 && status < 300;
-            }
 
-            function mergeHeaders(config) {
-                var reqHeaders = _.extend({}, config.headers);
-                var defHeaders = _.extend({},
-                    defaults.headers.common,
-                    defaults.headers[(config.method || 'get').toLowerCase()]
-                );
-
-                _.forEach(defHeaders, function (value, key) {
-                    var headerExists = _.some(reqHeaders, function (v, k) {
-                        return k.toLowerCase() === key.toLowerCase();
-                    });
-
-                    if (!headerExists) {
-                        reqHeaders[key] = value;
-                    }
-                });
-                return executeHeaderFns(reqHeaders, config);
-            }
-
-            function removeContentType(headers) {
-                _.forEach(config.headers, function (v, k) {
-                    if (k.toLowerCase() === 'content-type') {
-                        delete headers[k];
-                    }
-                });
-            }
-
-            function executeHeaderFns(headers, config) {
-                return _.transform(headers, function (result, v, k) {
-                    if (_.isFunction(v)) {
-                        v = v(config);
-                        if (_.isNull(v) || _.isUndefined(v)) {
-                            delete result[k];
-                        } else {
-                            result[k] = v;
-                        }
-                    }
-                }, headers);
-            }
-
-            function headersGetter(headers) {
-                // parse headerString
-                var headersObj;
-
-                return function (key) {
-                    headersObj = headersObj || parseHeaders(headers);
-                    return key ? headersObj[key.toLowerCase()] : headersObj;
-                };
-            }
-
-            function parseHeaders(headers) {
-                if (_.isObject(headers)) {
-                    return _.transform(headers, function (result, v, k) {
-                        result[_.trim(k.toLowerCase())] = _.trim(v);
-                    }, {});
-                }
-
-                var lines = headers.split('\n');
-                return _.transform(lines, function (result, line) {
-                    var pairs = line.split(':');
-                    var key = _.trim(pairs[0].toLowerCase());
-                    var value = _.trim(pairs[1]);
-                    if (key) {
-                        result[key] = value;
-                    }
-                }, {});
-            }
-
-            function transformData(data, headers, transformFn) {
-                if (_.isFunction(transformFn)) {
-                    return transformFn(data);
-                } else if (_.isArray(transformFn)) {
-                    return _.reduce(transformFn, function (preData, fn) {
-                        return _.isFunction(fn) ? fn(preData, headers) : preData;
-                    }, data);
-                } else {
-                    return data;
-                }
-            }
+            return d.promise;
         }
 
         $http.defaults = defaults;
 
         return $http;
+
+
+        // 这里为什么不考虑 302 的问题？
+        // 因为遇到了 302 ，浏览器会自行处理这个资源，不会去到我们的 js 代码中来处理。
+        function isSuccess(status) {
+            return status >= 200 && status < 300;
+        }
+
+        function mergeHeaders(config) {
+            var reqHeaders = _.extend({}, config.headers);
+            var defHeaders = _.extend({},
+                defaults.headers.common,
+                defaults.headers[(config.method || 'get').toLowerCase()]
+            );
+
+            _.forEach(defHeaders, function (value, key) {
+                var headerExists = _.some(reqHeaders, function (v, k) {
+                    return k.toLowerCase() === key.toLowerCase();
+                });
+
+                if (!headerExists) {
+                    reqHeaders[key] = value;
+                }
+            });
+            return executeHeaderFns(reqHeaders, config);
+        }
+
+        function executeHeaderFns(headers, config) {
+            return _.transform(headers, function (result, v, k) {
+                if (_.isFunction(v)) {
+                    v = v(config);
+                    if (_.isNull(v) || _.isUndefined(v)) {
+                        delete result[k];
+                    } else {
+                        result[k] = v;
+                    }
+                }
+            }, headers);
+        }
+
+        function headersGetter(headers) {
+            // parse headerString
+            var headersObj;
+
+            return function (key) {
+                headersObj = headersObj || parseHeaders(headers);
+                return key ? headersObj[key.toLowerCase()] : headersObj;
+            };
+        }
+
+        function parseHeaders(headers) {
+            if (_.isObject(headers)) {
+                return _.transform(headers, function (result, v, k) {
+                    result[_.trim(k.toLowerCase())] = _.trim(v);
+                }, {});
+            }
+
+            var lines = headers.split('\n');
+            return _.transform(lines, function (result, line) {
+                var pairs = line.split(':');
+                var key = _.trim(pairs[0].toLowerCase());
+                var value = _.trim(pairs[1]);
+                if (key) {
+                    result[key] = value;
+                }
+            }, {});
+        }
+
+        function transformData(data, headers, transformFn, status) {
+            if (_.isFunction(transformFn)) {
+                return transformFn(data, headers, status);
+            } else if (_.isArray(transformFn)) {
+                return _.reduce(transformFn, function (preData, fn) {
+                    return _.isFunction(fn) ? fn(preData, headers, status) : preData;
+                }, data);
+            } else {
+                return data;
+            }
+        }
     }];
 }
